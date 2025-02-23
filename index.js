3a6d70e0d14197f1b89e27b0d9b83d863448a229
benjamin2orie <ogechukwubenj@gmail.com>
2025-02-23 13:38:41 +0100

    path for upload

--- index.js
+++ index.js
@@ -4,15 +4,11 @@ import express from 'express';
 import cors from 'cors';
 import dbConnection from './src/model/db.connection.js';
 import router from './src/views/router.js';
+import path from 'path';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import swaggerUi from 'swagger-ui-express';
 import fs from 'fs';
-// import path from 'path';
-// import { fileURLToPath } from 'url';
-// import { dirname } from 'path';
-// import swaggerUi from 'swagger-ui-express';
-// import swaggerDocument from './swagger.json' assert {type: 'json'};
 
 
 dotenv.config();
@@ -34,7 +30,7 @@ app.use(cors({
 
 const swaggerDocument = JSON.parse(fs.readFileSync(__dirname + '/swagger.json', 'utf8'));
 
-// app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 const port = process.env.PORT || 3000;
 
 // Swagger UI setup
